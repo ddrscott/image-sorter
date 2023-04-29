64e0f49e96c95eb369f98f6556fce8ea127e63a6
Scott Pierce <ddrscott@gmail.com>
2023-04-29 15:47:42 -0500

    Add gaTag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 import type { AppProps } from 'next/app'
-
+const gaTag = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            {gaTag && <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gaTag}`} /> }
+            {gaTag && <Script id='google-analytics' strategy="afterInteractive" dangerouslySetInnerHTML={{
+                            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gaTag}');
+                            `,
+                            }}
+                    />}
+            <Component {...pageProps} />
+        </>
+    )
 }
 export default MyApp
